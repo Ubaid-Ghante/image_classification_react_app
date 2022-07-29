96a05260c2aba38aabab541cf6856b3eb2e3b407
Ubaid-Ghante <104935924+Ubaid-Ghante@users.noreply.github.com>
2022-07-29 15:17:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import '@tensorflow/tfjs-backend-cpu';
+import '@tensorflow/tfjs-backend-webgl';
+
+import * as mobilenet from '@tensorflow-models/mobilenet';
+
+
+import React, { useState, useEffect, useRef } from 'react';
+import './App.css';
+
+
+function App() {
+  const [isModelLoading, setIsModelLoading] = useState(false)
+  const [model, setModel] = useState(null)
+  const [imageURL, setImageURL] = useState(null)
+  const [results, setResults] = useState([])
+
+  const imageRef = useRef()
+
+  const loadModel = async () => {
+    setIsModelLoading(true)
+    try {
+        const model = await mobilenet.load()
+        console.log(model)
+        setModel(model)
+        setIsModelLoading(false)
+    }catch(error){
+      console.log(error)
+      setIsModelLoading(false)
+    }
+  }
+
+  const uploadImage = (e) => {
+    const {files} = e.target
+    if (files.length > 0){
+        const url = URL.createObjectURL(files[0])
+        setImageURL(url)
+    }else{
+      setImageURL(null)
+
+    }
+  }
+
+  const identify = async () => {
+    const results = await model.classify(imageRef.current)
+    setResults(results)
+  } 
+
+
+
+  useEffect(() => {
+    loadModel()
+  }, [])
+
+  if (isModelLoading){
+    return <h2>Model Loading....</h2>
+  }
+
+  console.log(results)
+
+  return (
+    <div className='App'>
+      <h1 className='header'>What Is It!!</h1>
+      <div className='inputHolder'>
+        <input type='file' accept='image/*' capture='camera' className='uploadInput'
+        onChange={uploadImage} />
+
+      </div>
+      <div className='mainWrapper'>
+        <div className='mainContent'>
+          <div className='imageHolder'>
+            {imageURL && <img src={imageURL} alt='Upload Preview' crossOrigin='anonymous' ref={imageRef} />}
+          </div>
+          {results.length > 0 && <div className='resultsHolder'>
+                        {results.map((result, index) => {
+                            return (
+                                <div className='result' key={result.className}>
+                                    <span className='name'>{result.className}</span>
+                                    <span className='confidence'> Confidence level: {(result.probability * 100).toFixed(2)}% {index === 0 && <span className='bestGuess'> Best Guess</span>}</span>
+                                </div>
+                            )
+                        })}
+            </div>}
+        </div>
+        {imageURL && <button className='button' onClick={identify} >Identify Image</button>}
+      </div>
+    </div>
+  );
+}
+
+export default App;
